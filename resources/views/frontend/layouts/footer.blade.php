478334a1117927f326341a18ad2b83de6af4ef75
dil-chhetri <dilchhetri1999@gmail.com>
2023-10-08 11:30:08 +0545

    integrated to end socket points

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -4,7 +4,8 @@
 
 <script>
     $(document).ready(function(){
-        $("#messageContainer").scrollTop($("#messageContainer")[0].scrollHeight);
+        getMessage();
+        
         $('#messageSent').on('submit',function(e){
             e.preventDefault();
             jQuery.ajax({
@@ -14,13 +15,36 @@
                 
                 success:function(response)
                 {
-                    $("#messageContainer").scrollTop($("#messageContainer")[0].scrollHeight);
                     jQuery('#messageSent')[0].reset();
+                    getMessage();
                 }
             });
 
 
         });
+        function getMessage(){
+            var id = {{request('id')}};
+            $.ajax({
+            type: 'GET',
+            url: '/chat/'+id, // Replace with your actual server endpoint
+            success: function (data) {
+                $('#messageContainer').empty();
+                $.each(data.messages, function(key,item){
+                    var messageClass = item.user_id === {{session('user_id')}}? 'bg-dark': 'bg-secondary';
+                    var messageStyle = item.user_id === {{session('user_id')}}? 'margin-left:auto;': '';
+                    $('#messageContainer').append('<div class="message '+messageClass+' px-2 py-1 mb-3  mt-3 rounded " style="width:max-content;'+messageStyle+'max-width:75%;">\
+                        <p class="text-center text-white">'+item.message+'</p>\
+                        </div>');
+                    $("#messageContainer").scrollTop($("#messageContainer")[0].scrollHeight);
+
+                });
+            },
+            error: function (xhr, status, error) {
+            console.error('Error fetching data:', error);
+            }
+            });
+        }
+
 
             function fetchData() {
             var id = {{request('id')}};
@@ -44,7 +68,7 @@
             }
 
             // Call the fetchData function initially
-            fetchData();
+            // fetchData();
 
             // Set up a recurring timer to fetch data every 3 seconds (3000 milliseconds)
             setInterval(fetchData, 3000);
